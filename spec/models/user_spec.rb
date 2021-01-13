5db8160ded578864ea02b48e3db785162c8266f1
tomotim <business.tim.78@gmail.com>
2021-01-13 16:10:44 +0900

    単体テストコード：修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe User, type: :model do
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
       another_user.valid?
-      expect(@user.errors.full_messages).to include(""Email has already been taken"]")
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
     end
 
     it "メールアドレスは、@を含む必要があること" do
