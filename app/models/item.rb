931b1cbf310bb7175e00074800c8d1ce1ee92fa6
tomotim <business.tim.78@gmail.com>
2021-01-15 18:09:32 +0900

    商品投稿機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,7 +21,7 @@ class Item < ApplicationRecord
 
     validates :product          
     validates :explanation      
-    validates :price
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
   end
 end
--- app/models/shipping_days.rb
+++ app/models/shipping_days.rb
@@ -1,4 +1,4 @@
-class Shipping_days < ActiveHash::Base
+class ShippingDays < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
     { id: 1, name: '1~2日で発送' },
