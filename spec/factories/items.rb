e517d46e11abad5fdd188e245f6dd5331f5298c7
tomotim <business.tim.78@gmail.com>
2021-01-15 20:34:51 +0900

    itemモデルの単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    product          { '商品名' }
+    explanation      { 'これは商品の説明です' }
+    ctegory_id       { Faker::Number.between(from: 1, to: 10) }
+    condtion_id      { Faker::Number.between(from: 1, to: 6) }
+    burden_id        { Faker::Number.between(from: 1, to: 2) }
+    shipping_area_id { Faker::Number.between(from: 1, to: 47) }
+    shipping_days_id { Faker::Number.between(from: 1, to: 3) }
+    price            { Faker::Number.between(from: 300, to: 9_999_999) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
