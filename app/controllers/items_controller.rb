a23f1e972918ff22ffbe6984901280f1cee18972
tomotim <business.tim.78@gmail.com>
2021-01-15 16:27:20 +0900

    (Js)販売手数料や販売利益の計算の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,7 +18,7 @@ class ItemsController < ApplicationController
 
   private
   def items_params
-    params.require(:item).permit(:content, :image, :product, :explanation, :ctegory_id, :condtion_id, :burden_id, :shipping_area_id, :shipping_days_id, :price ).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :product, :explanation, :ctegory_id, :condtion_id, :burden_id, :shipping_area_id, :shipping_days_id, :price ).merge(user_id: current_user.id)
   end
 
 end
---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+function calcPrice() {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = Math.floor(inputValue * 0.9);
+  });
+}
+
+window.addEventListener( "load", calcPrice );
