0a7ab7b7da46f1a90a596595477d7965ebb10e6c
tomotim <business.tim.78@gmail.com>
2021-01-17 21:28:07 +0900

    不正アクセスへの対策

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :login_user_origin?, only: :edit
+  before_action :login_user_origin?, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
