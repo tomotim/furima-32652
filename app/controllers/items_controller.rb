355ee52d35c2c5f33b674905e56e05b23ba1b3a8
tomotim <business.tim.78@gmail.com>
2021-01-29 06:20:11 +0900

    売却済み商品の編集ページへいけなくする修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :login_user_origin?, only: [:edit, :update, :destroy]
+  before_action :prevent_sold_item,  only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -53,4 +54,9 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
+  def prevent_sold_item
+    redirect_to root_path if @item.purchase.present?
+  end
+
 end
